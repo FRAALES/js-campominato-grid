0f763994cdaa068759b8d4fbb369338ee8f46a05
FRAALES <alessandrofranco1992@gmail.com>
2023-05-09 17:46:26 +0200

    codice tagliato

--- js/main.js
+++ js/main.js
@@ -2,39 +2,64 @@
 const gridElement = document.getElementById('test');
 console.log(gridElement);
 
-const arrayNum = writeNumber(1, 100);
-console.log(arrayNum);
+
+for (let i = 0; i < 100; i++){
+
+    const newSquare = createGridSquare("div", "square");
+
+    // newSquare.addEventListener("click",
+    //     function(){
+    //         newSquare.classList.add("clicked-true");
+    //         console.log(newSquare);
+    //     }
+    // ); 
+
+    gridElement.append(newSquare);
+}
+
+
+//Funzioni
+function createGridSquare(tagType, classToAdd){
+    const newElement = document.createElement(tagType);
+    newElement.classList.add(classToAdd);
+    return newElement;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const arrayNum = writeNumber(1, 100);
+// console.log(arrayNum);
 
 
 
 //L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 
 
-document.getElementById("test").style.display ="none";
+// document.getElementById("test").style.display ="none";
 
-function openTest() {
-    document.getElementById("test").style.display = "block";
-}
+// function openTest() {
+//     document.getElementById("test").style.display = "block";
+// }
 
-document.getElementById('btn').addEventListener('click', openTest);
+// document.getElementById('btn').addEventListener('click', openTest);
+
+
 
 //Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 //Ogni cella ha un numero progressivo, da 1 a 100.
-for (let i = 0; i < 100; i++){
 
-    const newSquare = createGridSquare("div", "square");
-
-    newSquare.addEventListener("click",
-        function(){
-            newSquare.classList.add("clicked-true");
-            console.log(newSquare);
-        }
-    ); 
-
-    gridElement.append(newSquare);
-
-
-}
 
 
 //Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
@@ -52,23 +77,17 @@ for (let i = 0; i < 100; i++){
 
 
 
-//Funzioni
-function createGridSquare(tagType, classToAdd){
-    const newElement = document.createElement(tagType);
-    newElement.classList.add(classToAdd);
-    return newElement;
-};
 
 
 
-//Funzione che crea i numeri da 1 a 100 in ordine crescente
-function writeNumber (min, max){
-    const numeriArr = [];
+// //Funzione che crea i numeri da 1 a 100 in ordine crescente e li stampa in console
+// function writeNumber (min, max){
+//     const numeriArr = [];
 
-    for (let i = 1; i <= 100; i++){
-        console.log([i]);
-    }
+//     for (let i = 1; i <= 100; i++){
+//         console.log([i]);
+//     }
 
-    return numeriArr; 
+//     return numeriArr; 
 
-}
+// }
